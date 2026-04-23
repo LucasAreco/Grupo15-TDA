daccf9c92a7ba2955a8fc8222daa7302f80bb889
melina <mretamozo@fi.uba.ar>
2026-04-23 11:36:59 -0300

    complete bt algorithm

--- backtracking/bt.py
+++ backtracking/bt.py
@@ -8,54 +8,63 @@ Las posiciones se indican en el formato (fila, columna, direccion)
 
 def get_next_pos(maze, position):
     row, col, direc = position
-    next_pos = None
-    if direc == "N" and row - 1 >= 0:
-        next_pos = (row - 1, col, direc)
-    elif direc == "S" and row + 1 < len(maze):
-        next_pos = (row + 1, col, direc)
-    elif direc == "W" and col - 1 >= 0:
-        next_pos = (row, col - 1, direc)
-    elif direc == "E" and col + 1 <= len(maze[0]):
-        next_pos = (row, col + 1, direc)
-    
-    if next_pos:
-        if maze[next_pos[0]][next_pos[1]] != "X":
-            return next_pos
+    row_mov, col_mov = 0, 0    
+
+    if direc  == "N":
+        row_mov -= 1
+    elif direc == "S":
+        row_mov += 1
+    elif direc == "W":
+        col_mov -= 1
+    elif direc == "E":
+        col_mov += 1
+
+    next_row = row + row_mov
+    next_col = col + col_mov
+
+    if (next_row >= 0 and next_row < len(maze)) and (next_col >= 0 and next_col < len(maze[0])):
+        if maze[next_row][next_col] != "X":
+            return (next_row, next_col, direc)
+
     return None
 
 def turn_left(position):
     row, col, direc = position
-    new_direc = direc
-    if direc == "N":
-        new_direc = "W"
-    elif direc == "S":
-        new_direc = "E"
-    elif direc == "W":
-        new_direc = "S"
-    else:
-        new_direc = "N"
 
-    return (row, col, new_direc)
+    if direc == "N": return (row, col, "W")
+    elif direc == "W": return (row, col, "S")
+    elif direc == "S": return (row, col, "E")
+    else: return (row, col, "N")
     
 def find_next_step(maze, pos, paths, visited):
     row, col, _ = pos
-    if pos in visited: return None
-    if maze[row][col] == "S": return paths
+    if maze[row][col] == "S": return [pos]
     
-    visited.add(pos)
+    visited.add((row, col))
 
     sol = None
     next_pos = get_next_pos(maze, pos)
-    if next_pos:
+    if next_pos and (next_pos[0], next_pos[1]) not in visited:
         print(f"Avanzo desde {pos} a {next_pos}")
         paths[(next_pos[0], next_pos[1])] = (pos[0], pos[1])  
         sol = find_next_step(maze, next_pos, paths, visited)
+        if sol:
+            return [pos] + sol
+
+    turn_left_pos = pos
+    for _ in range(3): 
+        turn_left_pos = turn_left(turn_left_pos)
+        print(f"Giro a {turn_left_pos}")
+
+        next_pos = get_next_pos(maze, turn_left_pos)
+        if next_pos and (next_pos[0], next_pos[1]) not in visited:
+            print(f"Avanzo desde {pos} a {next_pos}")
+            paths[(next_pos[0], next_pos[1])] = (pos[0], pos[1])  
+            sol = find_next_step(maze, next_pos, paths, visited)
+            if sol:
+                return [pos] + sol
     
-    if not sol:
-        next_pos = turn_left(pos)
-        print(f"Giro desde {pos} a {next_pos}")
-        sol = find_next_step(maze, next_pos, paths, visited)
-    
+    visited.remove((row, col))
     return sol
 
 def find_path(maze, entry): 
